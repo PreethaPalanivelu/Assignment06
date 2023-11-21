ce367635c234e94cdb714bad7f3e07168094f915
PreethaPalanivelu <mailtopreethaa@gmail.com>
2023-11-21 07:47:13 +0530

    assignment3

---
+++ src/main/java/com/example/studentms/model/Student.java
@@ -0,0 +1,45 @@
+package com.example.studentms.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Student
+{
+    @Id
+    //defining id as column name
+    @Column
+    private int id;
+    @Column
+    private String student_name;
+    //defining age as column name
+    @Column
+    private String course_name;
+    //defining email as column name
+    @Column
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getStudent_name() {
+        return student_name;
+    }
+    public void setStudent_name(String student_name) {
+        this.student_name = student_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
